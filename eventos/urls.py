76b251581de9c294e77818e39d7d912d9a8b1f23
Ivan Pedro dos S. Pinheiro <ivanpsan.san@gmail.com>
2026-08-14 15:20:46 -0300

    agenda finalizada

--- eventos/urls.py
+++ eventos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.mostrar_home, name="home"),
-        path("editar/<int:id>/", views.editar_evento, name="editar_evento"),
+    path("editar/<int:id>/", views.editar_evento, name="editar_evento"),
     path("excluir/<int:id>/", views.excluir_evento, name="excluir_evento"),
 ]
