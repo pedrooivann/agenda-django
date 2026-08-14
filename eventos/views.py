d1cd2efd1a7518fdb48bc39c7526e50665eb33c2
Ivan Pedro dos S. Pinheiro <ivanpsan.san@gmail.com>
2026-08-13 21:38:20 -0300

    whatever is happening here

--- eventos/views.py
+++ eventos/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from .models import Evento
 
-def mostrar_home (request):
-    return render(request, "index.html")
+def mostrar_home(request):
+
+    if request.method == "POST":
+        print("POST RECEBIDO:", request.POST)
+
+        titulo = request.POST["titulo"]
+        descricao = request.POST["descricao"]
+        data_evento = request.POST["data_evento"]
+        horario = request.POST["horario"]
+
+        Evento.objects.create(
+            titulo=titulo,
+            descricao=descricao,
+            data_evento=data_evento,
+            horario=horario
+        )
+
+    eventos = Evento.objects.all()
+
+    return render(request, "index.html", {
+        "eventos": eventos
+    })
