c1b5e7e9c1639fec2431aab1d2a42896807faa60
Ivan Pedro dos S. Pinheiro <ivanpsan.san@gmail.com>
2026-08-14 14:57:18 -0300

    eventos fizalizado

--- eventos/urls.py
+++ eventos/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mostrar_home, name="home")
+    path('', views.mostrar_home, name="home"),
+        path("editar/<int:id>/", views.editar_evento, name="editar_evento"),
+    path("excluir/<int:id>/", views.excluir_evento, name="excluir_evento"),
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Evento
 
+
 def mostrar_home(request):
 
     if request.method == "POST":
-        print("POST RECEBIDO:", request.POST)
 
         titulo = request.POST["titulo"]
         descricao = request.POST["descricao"]
@@ -20,10 +20,37 @@ def mostrar_home(request):
 
         return redirect("home")
 
-
-    eventos = Evento.objects.all()
+    eventos = Evento.objects.all().order_by("data_evento", "horario")
 
     return render(request, "index.html", {
         "eventos": eventos
     })
 
+
+def editar_evento(request, id):
+
+    evento = Evento.objects.get(id=id)
+
+    if request.method == "POST":
+
+        evento.titulo = request.POST["titulo"]
+        evento.descricao = request.POST["descricao"]
+        evento.data_evento = request.POST["data_evento"]
+        evento.horario = request.POST["horario"]
+
+        evento.save()
+
+        return redirect("home")
+
+    return redirect("home")
+
+
+def excluir_evento(request, id):
+
+    if request.method == "POST":
+
+        evento = Evento.objects.get(id=id)
+
+        evento.delete()
+
+    return redirect("home")
